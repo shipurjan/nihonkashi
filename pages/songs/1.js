1f42392c1f314eb8e0a62092ee6dd491e93cebb7
Cyprian Zdebski <57770826+shipurjan@users.noreply.github.com>
2021-12-05 19:16:04 +0100

    Changed song files to take songdata id manually

--- pages/songs/1.js
+++ pages/songs/1.js
@@ -1,16 +1,9 @@
 import Song from '../../components/Song'
 import songdata from './json/songlist.json'
-import { useRouter } from 'next/router'
 
-function truncPath(path){
-    let position = path.lastIndexOf("/")+1;
-    path = path.slice(position);
-    return path
-}
 
 export default function Songpage() {
-    const { asPath } = useRouter();
     return <Song songdata={
-        songdata[truncPath(asPath)]
+        songdata[1]
     } />
 }
