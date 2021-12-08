5a8c0f33db789d16e99b1c0b8bb1e71ed83e08b4
Cyprian Zdebski <57770826+shipurjan@users.noreply.github.com>
2021-12-08 16:26:02 +0100

    Added song sorting by romanized_artist and then romanized_title

--- components/Songlist.js
+++ components/Songlist.js
@@ -5,6 +5,16 @@ import songlist from '../pages/songs/json/songlist.json';
 
 function listSongs(list){
   let newlist = list.filter(e => e.id >= 0);
+  newlist.sort((a, b) => { // Sort alphabetically first by romanized_artist and then by romanized_title
+    let retval = 0;
+    if (a.romanized_artist < b.romanized_artist)
+      retval = -1;
+    if (a.romanized_artist > b.romanized_artist)
+      retval = 1;
+    if (retval === 0)
+      retval = a.romanized_title > b.romanized_title ? -1 : 1;
+    return retval;
+  })
   return newlist.map((data) => {
     return <li key={(data.id).toString()}>
         <Link href={"/songs/"+(data.id).toString()}>
