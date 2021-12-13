46989d29fab7bb4f99bb5b98e550b7f69a0518d3
Cyprian Zdebski <57770826+shipurjan@users.noreply.github.com>
2021-12-13 01:27:07 +0100

    Added deployments links to the footer

--- components/Footer.js
+++ components/Footer.js
@@ -6,17 +6,27 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.imageContainer}>
-      <Link href="https://github.com/shipurjan/nihonkashi">
-        <a><Image className={styles.image}
-        src="/github.png"
-        height={20}
-        width={20}
-        alt=""
-      /></a>
-      </Link>
+        <Link href="https://github.com/shipurjan/nihonkashi">
+          <a><Image className={styles.image}
+          src="/github.png"
+          height={20}
+          width={20}
+          alt=""
+        /></a>
+        </Link>・2021
       </div>
       <div>
-        2021
+        Deployments:
+        <div>
+          <Link href="https://nihonkashi.vercel.app/">
+          <a>master</a>
+          </Link>
+        </div>
+        <div>
+          <Link href="https://nipponkashi.vercel.app/">
+          <a>develop</a>
+          </Link>
+        </div>
       </div>
     </footer>
   )
